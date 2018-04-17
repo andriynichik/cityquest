366b67f5af8fb9d328f8c762a42e50e6a6548489
Андрій Ничик <andriy@MacBook-Air-Andrij.local>
2018-04-18 01:24:53 +0300

    db beckup and change settig

--- code/untitled.py
+++ code/untitled.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, render_template, flash, redirect, url_for
 import random
-from oauth import OAuthSignIn
+#  from oauth import OAuthSignIn
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
 
@@ -47,30 +48,29 @@ def index():
 
 @app.route('/logout')
 def logout():
-    logout_user()
     return redirect(url_for('index'))
 
 
-@app.route('/authorize/<provider>')
-def oauth_authorize(provider):
-    if not current_user.is_anonymous:
-        return redirect(url_for('index'))
-    oauth = OAuthSignIn.get_provider(provider)
-    return oauth.authorize()
+#@app.route('/authorize/<provider>')
+#def oauth_authorize(provider):
+#    if not current_user.is_anonymous:
+#        return redirect(url_for('index'))
+#    oauth = OAuthSignIn.get_provider(provider)
+#    return oauth.authorize()
 
 
 @app.route('/callback/<provider>')
 def oauth_callback(provider):
     if not current_user.is_anonymous:
         return redirect(url_for('index'))
-    oauth = OAuthSignIn.get_provider(provider)
-    social_id, username, email = oauth.callback()
-    if social_id is None:
+#    oauth = OAuthSignIn.get_provider(provider)
+#    social_id, username, email = oauth.callback()
+#    if social_id is None:
         flash('Authentication failed.')
         return redirect(url_for('index'))
-    user = User.query.filter_by(social_id=social_id).first()
-    if not user:
-        user = User(social_id=social_id, nickname=username, email=email)
+#    user = User.query.filter_by(social_id=social_id).first()
+#    if not user:
+ #       user = User(social_id=social_id, nickname=username, email=email)
         db.session.add(user)
         db.session.commit()
     login_user(user, True)
@@ -84,7 +84,33 @@ def home():
 
 @app.route('/maps')
 def maps():
+    regions = {
 
+        '1': 'Харківська область',
+        '2': 'Полтавська область',
+        '3': 'Київська область',
+        '4': 'Вінницька область',
+        '5': 'Житомирська область',
+        '6': 'Закарпатська область',
+        '7': 'Хмельницька область',
+        '8': 'АР Крим',
+        '9': 'Запорізька область',
+        '10': 'Херсонська область',
+        '11': 'Одеська область',
+        '12': 'Дніпропетровська область',
+        '13': 'Чернігівська область',
+        '14': 'Тернопільська область',
+        '15': 'Донецька область',
+        '16': 'Сумська область',
+        '17': 'Кіровоградська область',
+        '18': 'Черкаська область',
+        '19': 'Миколаївська область',
+        '20': 'Рівненська область',
+        '21': 'Львівська область',
+        '22': 'Волинська область',
+        '24': 'Івано-Франківська область',
+        '25': 'Чернівецька область',
+    }
     points = [
               [{"lat": "50.5286636209", "lng": "27.8823074965"}],
               [{"lat": "48.4968544176", "lng": "32.241026259"}],
@@ -154,7 +180,7 @@ def maps():
     ]
     data = random.choice(points)
     print(data)
-    return render_template('maps.html', data=data)
+    return render_template('maps.html', data=data, regions=regions)
 
 if __name__ == '__main__':
     app.run(debug=True,  host='0.0.0.0')

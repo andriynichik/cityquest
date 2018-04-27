ecd3b71c10f2f1c15e2ea8da8c04acb14e487d46
AN <an@localhost.localdomain>
2018-04-27 19:08:06 +0300

    fix auth

--- app/manage.py
+++ app/manage.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, flash, redirect, url_for, request
 import random
 import  os
 import sys
-#  from oauth import OAuthSignIn
+from oauth import OAuthSignIn
 import json
 from flask_sqlalchemy import SQLAlchemy
 
@@ -25,8 +25,8 @@ manager.add_command('db', MigrateCommand)
 
 app.config['OAUTH_CREDENTIALS'] = {
     'facebook': {
-        'id': '205733200185301',
-        'secret': '3d4cf797e215984bd3ad7935bc5ed335'
+        'id': '156164541888335',
+        'secret': 'cc23f5004b2e5536218a3206635b75bc'
     },
     'twitter': {
         'id': '3RzWQclolxWZIMq5LJqzRZPTl',
@@ -58,28 +58,28 @@ def logout():
     return redirect(url_for('index'))
 
 
-#@app.route('/authorize/<provider>')
-#def oauth_authorize(provider):
-#    if not current_user.is_anonymous:
-#        return redirect(url_for('index'))
-#    oauth = OAuthSignIn.get_provider(provider)
-#    return oauth.authorize()
+@app.route('/authorize/<provider>')
+def oauth_authorize(provider):
+   if not current_user.is_anonymous:
+       return redirect(url_for('index'))
+   oauth = OAuthSignIn.get_provider(provider)
+   return oauth.authorize()
 
 
 @app.route('/callback/<provider>')
 def oauth_callback(provider):
     if not current_user.is_anonymous:
         return redirect(url_for('index'))
-#    oauth = OAuthSignIn.get_provider(provider)
-#    social_id, username, email = oauth.callback()
-#    if social_id is None:
+    oauth = OAuthSignIn.get_provider(provider)
+    social_id, username, email = oauth.callback()
+    if social_id is None:
         flash('Authentication failed.')
         return redirect(url_for('index'))
-#    user = User.query.filter_by(social_id=social_id).first()
-#    if not user:
- #       user = User(social_id=social_id, nickname=username, email=email)
-        db.session.add(user)
-        db.session.commit()
+    user = User.query.filter_by(social_id=social_id).first()
+    if not user:
+       user = User(social_id=social_id, nickname=username, email=email)
+       db.session.add(user)
+       db.session.commit()
     login_user(user, True)
     return redirect(url_for('index'))
   
